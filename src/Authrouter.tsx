71ab90ec0b34488fd381dbf15b0bbe0b49d6eeb9
zldymlg <zaldymalig@gmail>
2025-03-18 23:37:00 +0800

    Router

--- src/Authrouter.tsx
+++ src/Authrouter.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import AuthPage from "./Auth";
 import Content from "./AppContent";
 
